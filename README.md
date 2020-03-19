02ca667e36c29f49eaac8bccff0db5eb83830e5e
tiderjian <tider@quansitech.com>
2020-03-19 15:09:57 +0800

    增加wss协议

--- README.md
+++ README.md
@@ -9,8 +9,8 @@ composer require quansitech/qrscanner
 #### 使用
 启动服务端程序
 ```php
-// -d 启动守护进程  2346 为端口号（必填）  100为二维码的过期时间（必填）
-./vendor/bin/qrscanner start -d 2346 100  
+// --d 启动守护进程(选填)  --config=配置文件绝对路径(选填) 如果不填 则host默认0.0.0.0 port默认2346 ssl默认不启动
+./vendor/bin/qrscanner start --d --config=/var/www/.env
 ```
 
 停止服务
@@ -21,6 +21,16 @@ composer require quansitech/qrscanner
 
 重启服务程序
 ```php
-// -d 启动守护进程 (选填)  2346 为端口号（必填）  100为二维码的过期时间（必填）
-./vendor/bin/qrscanner restart -d 2346 100  
+// --d 启动守护进程 (选填)  --config=配置文件绝对路径(选填)
+./vendor/bin/qrscanner restart --d --config=/var/www/.env
+```
+
+配置文件
+```php
+host=0.0.0.0
+port=2346
+expire=300 //二维码过期时间，单位秒，不填则默认300秒
+ssl=false //是否开启ssl
+ssl_cert=/var/www/fullchain1.pem //指定ssl 证书
+ssl_pk=/var/www/privkey1.pem //指定ssl key
 ```
--- src/Ws.php
+++ src/Ws.php
@@ -1,6 +1,7 @@
 <?php
 namespace Qrscanner;
 
+use Dotenv\Dotenv;
 use Workerman\Worker;
 
 class Ws{
@@ -8,32 +9,70 @@ class Ws{
     static $map = [];
     static $expire = 300;
 
+    static $default = [
+        'port' => 2346,
+        'host' => '0.0.0.0',
+        'ssl' => false
+    ];
+
+    static protected function parseConfig(){
+        global $argv;
+
+        $config['method'] = $argv[1];
+        unset($argv[1]);
+        foreach($argv as $v){
+            if(strpos($v, '--') == 0){
+                $v = ltrim($v, "--");
+                list($key, $value) = explode("=", $v);
+                $config[$key] = is_null($value) ? true : $value;
+            }
+        }
+
+        return $config;
+    }
+
     static function open(){
         global $argv;
 
-        if($argv[1] == 'stop'){
+        $config = self::parseConfig();
+        $argv[1] = $config['method'];
+
+        if($config['d']){
+            $argv[2] = '-d';
+        }
+
+        if($config['method'] == 'stop'){
             Worker::runAll();
             return;
         }
 
-        if(!is_integer($argv[2])){
-            $port = $argv[3];
-            self::$expire = $argv[4];
-            unset($argv[3]);
-            unset($argv[4]);
+        $path = dirname($config['config']);
+        $file = str_replace($path . '/', '', $config['config']);
+
+        $dotenv = Dotenv::create($path, $file);
+        $dotenv->load();
+
+        $ws_worker = null;
+        $host = env('host', self::$default['host']);
+        $port = env('port', self::$default['port']);
+        $ssl = env('ssl', self::$default['ssl']);
+        self::$expire = env('expire', self::$expire);
+        if($ssl){
+            $context = array(
+                'ssl' => array(
+                    'local_cert'                 => env('ssl_cert'),
+                    'local_pk'                   => env('ssl_pk'),
+                    'verify_peer'                => false,
+                    'allow_self_signed' => true
+                )
+            );
+
+            $ws_worker = new Worker("websocket://{$host}:{$port}", $context);
+            $ws_worker->transport = 'ssl';
         }
         else{
-            $port = $argv[2];
-            self::$expire = $argv[3];
-            unset($argv[2]);
-            unset($argv[3]);
+            $ws_worker = new Worker("websocket://{$host}:{$port}");
         }
-
-
-
-
-        // Create a Websocket server
-        $ws_worker = new Worker("websocket://0.0.0.0:{$port}");
 
         $ws_worker->onMessage = function($connection, $data)
         {
